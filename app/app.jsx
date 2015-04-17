2566cb19bdc681563568fb861b0df20cdd80d512
Chris Mather <chris@eventedmind.com>
2015-04-17 14:33:02 -0700

    working with components

--- app/app.jsx
+++ app/app.jsx
@@ -1,14 +1,35 @@
-children = [
-  React.createElement("li", {}, "Hello"),
-  React.createElement("li", {}, "World")
-];
+class List extends React.Component {
+  constructor(props) {
+    this.state = {name: 'World'};
+  }
 
-listElement = React.createElement("ul", {
-  customProp: "some value",
-  className: "my-list",
-  onClick: function (e) {
+  handleClick(e) {
     console.log(e);
   }
-}, children);
 
-React.render(listElement, document.body);
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  render() {
+    console.log("Rendering React Component");
+
+    var name = this.state.name;
+
+    var children = [
+      React.createElement("li", {}, "Hello"),
+      React.createElement("li", {}, name)
+    ];
+
+    var listElement = React.createElement("ul", {
+      customProp: "some value",
+      className: "my-list",
+      onClick: (e) => this.handleClick(e)
+    }, children);
+
+    return listElement;
+  }
+}
+
+var listElement = React.createElement(List);
+component = React.render(listElement, document.body);
